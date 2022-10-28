90902a1bfe173330ef50cf345dd5ffd48512d290
Vicente Martinez <->
2022-10-28 11:59:36 -0400

    Supplier CRUD

--- InventoryManager/src/Supplier/supplier.js
+++ InventoryManager/src/Supplier/supplier.js
@@ -1,28 +1,41 @@
-const suppliers =[
-    {supplierID:1, supplierName:"OE Sypply", supplierDetail:"Battery and oil", supplierStreet:"25 Glendale Ave.", supplierCity:"St. Catherines", supplierProvince:"ON", supplierPostalCode:"L1S1J2", supplierPhone:"222-000-1555", supplierExt:"456", supplierEmail:"supplier@oesupply.ca", supplierCreatedAt:"2022-01-2021", supplierUpdatedAt:"2022-09-20"},
-    {supplierID:2, supplierName:"Ontario's Lawn Mowers", supplierDetail:"Lawn Mowers", supplierStreet:"475 Glendale Ave.", supplierCity:"St. Catherines", supplierProvince:"ON", supplierPostalCode:"L1S1G9", supplierPhone:"456-557-8877", supplierExt:"045", supplierEmail:"ckarl@olm.ca", supplierCreatedAt:2019-04-08, supplierUpdatedAt:2022-04-07},
-    {supplierID:3, supplierName:"Bardon Supplies Ltd.", supplierDetail:"Engine supplies", supplierStreet:"21 Road Stone Rd.", supplierCity:"Niagara On The Lake", supplierProvince:"ON", supplierPostalCode:"L0S1J0", supplierPhone:"789-000-0498", supplierExt:"", supplierEmail:"supplier@bardon.ca", supplierCreatedAt:2021-04-15, supplierUpdatedAt:2021-12-21},
-    {supplierID:4, supplierName:"TML Supply Company", supplierDetail:"Lawn Mowers", supplierStreet:"590 Welland Ave", supplierCity:"Niagara Falls", supplierProvince:"On", supplierPostalCode:"L1L1P3", supplierPhone:"122-040-9898", supplierExt:"446", supplierEmail:"cken@tml.ca", supplierCreatedAt:2015-06-07, supplierUpdatedAt:2020-07-08},
-    {supplierID:5, supplierName:"Flexo Products Limited", supplierDetail:"Car supplies", supplierStreet:"4650 Montrose Rd.", supplierCity:"Niagara Falls", supplierProvince:"ON", supplierPostalCode:"L5F4G5", supplierPhone:"998-582-4050", supplierExt:"77", supplierEmail:"bdiaz@flexoproducts.ca", supplierCreatedAt:2020-05-17, supplierUpdatedAt:2021-01-04},
+const supplierTableF = {
+};
 
-]
-
-
-function filterTableSupplier() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+supplierTableF.initTable = () => {
+    // Example to get data from query param
+    if (globalData.searchParams.has("selectId")) {
+        //alert("Escogiste " + globalData.personData.findById(globalData.searchParams.get("selectId")).fullName);
     }
+
+    supplierTableF.supplierTable = new TableActions(
+        "supplierTable",
+        [ "supplierName", "supplierDetail", "supplierPostalCode", "supplierPhone", "supplierEmail"],
+        "supplierID"
+    );
+    supplierTableF.supplierTable.addActionEdit("Main.html?page=newSupplier");
+    supplierTableF.supplierTable.addActionView("Main.html?page=newSupplier");
+    supplierTableF.supplierTable.addActionDelete(
+        globalData.supplierData,
+        () => supplierTableF.filerTable()
+    );
+    // supplierTableF.supplierTable.addAction({ css: "fa fa-trash red", redirectTo: "IndexPerson.html", title: "Delete" });
+    // supplierTableF.supplierTable.addAction({ css: "fa fa-search", redirectTo: "IndexPerson.html", title: "View" });
+
+    supplierTableF.supplierTable.refresh(globalData.supplierData.get());
 }
+
+supplierTableF.filerTable = () => {
+    let input = document.getElementById("myInput");
+    let filter = input.value.toUpperCase();
+
+    let jsonFiler = globalData.supplierData.get()
+        .filter(s => {
+            return s.supplierName.toString().toUpperCase().includes(filter) ||
+                s.supplierDetail.toString().toUpperCase().includes(filter) ||
+                s.supplierPostalCode.toString().toUpperCase().includes(filter) ||
+                s.supplierPhone.toString().toUpperCase().includes(filter) ||
+                s.supplierEmail.toString().toUpperCase().includes(filter) 
+        });
+
+    supplierTableF.supplierTable.refresh(jsonFiler);
+};
---
+++ InventoryManager/src/Supplier/viewsupplier.js
@@ -0,0 +1,79 @@
+const supplierTableF = {
+    mode: 'new',
+    supplier: {}
+};
+
+supplierTableF.delete = () => {
+    crudDelete(globalData.supplierData, null, supplierTableF.supplier, () => {
+        window.location.href = "../Main/Main.html?page=suppliers";
+    });
+}
+
+supplierTableF.cancel = () => {
+    window.location.href = "../Main/Main.html?page=suppliers";
+}
+
+supplierTableF.edit = () => {
+    editDataForm();
+    document.getElementById("btnEdit").hidden = true;
+    document.getElementById("btnSave").hidden = false;
+}
+
+supplierTableF.save = () => {
+    // Add When was updated
+    supplierTableF.supplier.updateAt = getDateTimeFormat(new Date());
+    // Read Field from form and mapping into object
+    mapFormToObject(supplierTableF.supplier);
+    // Valid is a new Object or Update
+    if (supplierTableF.mode == "new") {
+        globalData.supplierData.insert(supplierTableF.supplier);
+        alert("Supplier created successfully!!!");
+    } else {
+        globalData.supplierData.update(supplierTableF.supplier);
+        alert("Supplier updated successfully!!!");
+    }
+    // Send to list
+    window.location.href = "../Main/Main.html?page=suppliers";    
+}
+
+supplierTableF.init = () => {
+    if (globalData.searchParams.has("mode")) {
+        let tmp = globalData.searchParams.get("mode");
+        if (tmp) {
+            supplierTableF.mode = tmp;
+        }
+    }
+
+// Add Valid form
+const form = document.getElementById("formSupplier");
+addValidationForm(form, supplierTableF.save);
+
+// Check Mode
+if (supplierTableF.mode == "new") {
+    editDataForm();
+    // Show Save Btn
+    document.getElementById("btnSave").hidden = false;
+} else {
+    document.getElementById("title").textContent = "View Supplier";
+    document.title = "View Supplier";
+    // Show Delete Btn
+    document.getElementById("btnDelete").hidden = false;
+
+    // Load Supplier from parameter
+    supplierTableF.supplier = loadDataSelected(globalData.supplierData,  "../Main/Main.html?page=suppliers");
+    if (!supplierTableF.supplier) {
+        return;
+    }
+
+    if (supplierTableF.mode == "edit") {
+        supplierTableF.edit();
+    } else {
+        // SHow Edit Btn and Hidden Save
+        document.getElementById("btnEdit").hidden = false;
+        document.getElementById("btnSave").hidden = true;
+    }
+
+    // Fill all data
+    fillFormData(supplierTableF.supplier);
+    }
+}
